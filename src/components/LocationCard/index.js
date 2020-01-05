b907ff8b1df382316e6850aa3a92b0c21bb99043
Pavel Vashkel <p.vashkel@datarockets.com>
2020-01-05 11:37:09 +0300

    Add LocationCard component

---
+++ src/components/LocationCard/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Row, TextContent, TextTitle, TouchableCard, WrapTitle } from './styled'
+
+const LocationCard = ({ location, onPress }) => {
+  return (
+    <TouchableCard onPress={() => onPress(location.id)}>
+      <WrapTitle>
+        <TextTitle>{location.name}</TextTitle>
+      </WrapTitle>
+      <Row>
+        <TextContent>Type</TextContent>
+        <TextContent>{location.type}</TextContent>
+      </Row>
+      <Row>
+        <TextContent>Dimension</TextContent>
+        <TextContent>{location.dimension}</TextContent>
+      </Row>
+    </TouchableCard>
+  )
+}
+
+const { number, string, func, shape } = PropTypes
+
+LocationCard.propTypes = {
+  location: shape({
+    id: number,
+    name: string,
+    type: string,
+    dimension: string,
+  }).isRequired,
+  onPress: func,
+}
+
+LocationCard.defaultProps = {
+  onPress: () => null,
+}
+
+export default LocationCard
--- src/screens/LocationsScreen/index.js
+++ src/screens/LocationsScreen/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import LocationCard from '../../components/LocationCard'
+
 const LocationsScreen = () => {
   return (
     <View>
       <Text>
         LocationsScreen
       </Text>
+      <LocationCard
+        location={{
+          id: 1,
+          name: 'Earth (C-137)',
+          type: 'Planet',
+          dimension:' Dimension C-137',
+        }}
+      />
     </View>
   )
 }
